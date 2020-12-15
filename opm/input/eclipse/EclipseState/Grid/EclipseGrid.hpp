96ce160cf661702c031ec8f4ae95e036a02ef6c6
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2020-12-15 09:28:10 +0100

    x

--- opm/input/eclipse/EclipseState/Grid/EclipseGrid.hpp
+++ opm/input/eclipse/EclipseState/Grid/EclipseGrid.hpp
@@ -38,15 +38,21 @@
 namespace Opm {
 
     class Deck;
-    namespace EclIO {
-        class EclFile;
-        class EclOutput;
-    }
     struct NNCdata;
     class UnitSystem;
     class ZcornMapper;
     class EclipseGridLGR;
     class LgrCollection;
+
+} // namespace Opm
+
+namespace Opm { namespace EclIO {
+    class EclFile;
+    class EclOutput;
+}} // namespace Opm::EclIO
+
+namespace Opm {
+
     /**
        About cell information and dimension: The actual grid
        information is held in a pointer to an ERT ecl_grid_type
