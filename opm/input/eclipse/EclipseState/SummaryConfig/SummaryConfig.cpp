feb2c5b4c0d62612d751ab09a8958c48358a66c2
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2024-07-17 10:52:47 +0200

    x

--- opm/input/eclipse/EclipseState/SummaryConfig/SummaryConfig.cpp
+++ opm/input/eclipse/EclipseState/SummaryConfig/SummaryConfig.cpp
@@ -2418,4 +2418,9 @@ void SummaryConfig::handleProcessingInstruction(const std::string& keyword)
     }
 }
 
+bool wantNormalisedConcentrationVariation(const SummaryConfig& smryCfg)
+{
+    return smryCfg.match("RCVAR*");
+}
+
 } // namespace Opm
