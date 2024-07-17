77de9639d679f3c4d95a091691fd350edb17f282
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2024-07-17 10:52:47 +0200

    x

--- opm/input/eclipse/EclipseState/SummaryConfig/SummaryConfig.hpp
+++ opm/input/eclipse/EclipseState/SummaryConfig/SummaryConfig.hpp
@@ -692,6 +692,8 @@ namespace Opm {
         void handleProcessingInstruction(const std::string& keyword);
     };
 
+    bool wantNormalisedConcentrationVariation(const SummaryConfig& smryCfg);
+
 } // namespace Opm
 
 #endif // OPM_SUMMARY_CONFIG_HPP
